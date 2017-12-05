116dc510f4c7868489a02dfbc2df29e0a9634d97
elinardo10 <elinardosilva@gmail.com>
2017-12-05 18:54:17 -0300

    start projetct

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -85,7 +85,7 @@
         </div>
       </div>
       <div class="copyrights text-center">
-        <p>Desenvolvido na <a href="" class="external">E-tech</a></p>
+        <p>Desenvolvido na <a href="#" class="float-center"><img src="img/logo_etech.png" style="max-height:30px;" class="img-fluid" alt=""></a>
         <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
       </div>
     </div>
